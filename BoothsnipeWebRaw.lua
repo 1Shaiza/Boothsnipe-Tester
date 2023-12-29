11aa2d144ce53c0c61da7f24c159a12c7a079250
1Shaiza <154394911+1Shaiza@users.noreply.github.com>
2023-12-29 09:16:56 -0600

    Update BoothsnipeWebRaw.lua

--- BoothsnipeWebRaw.lua
+++ BoothsnipeWebRaw.lua
@@ -1,5 +1,96 @@
 local Booths_Broadcast = game:GetService("ReplicatedStorage").Network:WaitForChild("Booths_Broadcast")
 
+if not getgenv().a then
+    getgenv().a = true
+    local vu = game:GetService("VirtualUser")
+    game:GetService("Players").LocalPlayer.Idled:connect(function()
+        vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+        wait(1)
+        vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+    end)
+end
+
+local function processListingInfo(uid, gems, item, version, shiny, amount, boughtFrom)
+    print(uid, gems, item, version, shiny, amount, boughtFrom)
+    print("BOUGHT FROM:", boughtFrom)
+    print("UID:", uid)
+    print("GEMS:", gems)
+    print("ITEM:", item)
+    local snipeMessage = game.Players.LocalPlayer.Name .. " just sniped a "
+    if version then
+        if version == 2 then
+            version = "Rainbow"
+        elseif version == 1 then
+            version = "Golden"
+        end
+    else
+       version = "Normal"
+    end
+    
+    snipeMessage = snipeMessage .. version
+    
+    if shiny then
+        snipeMessage = snipeMessage .. " Shiny"
+    end
+    
+    snipeMessage = snipeMessage .. " " .. (item)
+    
+    print(snipeMessage)
+    
+    if amount then
+        print("AMOUNT:", amount)
+    else
+        amount = 1
+        print("AMOUNT:", amount)
+    end
+
+    local fields = {
+        {
+            name = "PRICE:",
+            value = tostring(gems) .. " GEMS",
+            inline = true,
+        },
+        {
+            name = "BOUGHT FROM:",
+            value = tostring(boughtFrom),
+            inline = true,
+        },
+        {
+            name = "AMOUNT:",
+            value = tostring(amount),
+            inline = true,
+        },
+        {
+            name = "PETID:",
+            value = tostring(uid),
+            inline = true,
+        }
+    }
+
+    local message = {
+        content = "@everyone",
+        embeds = {
+            {
+                title = snipeMessage,
+                fields = fields,
+                author = {name = "New Pet Sniped!"}
+            }
+        },
+        username = "piratesniper",
+        attachments = {}
+    }
+
+    local http = game:GetService("HttpService")
+    local jsonMessage = http:JSONEncode(message)
+
+    http:PostAsync(
+        "https://discord.com/api/webhooks/1186432138195779698/I-MN6VBwVkTlpqC49yRMveog5pnA1tb4ZaLI3GLGpW1lWcOgQVXaMJAtR1TGeBVB1Ozx",
+        jsonMessage,
+        Enum.HttpContentType.ApplicationJson,
+        false
+    )
+end
+
 local function checklisting(uid, gems, item, version, shiny, amount, username, playerid)
     gems = tonumber(gems)
 
