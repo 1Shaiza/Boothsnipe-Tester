99b425986702927bc41ef9dbdc064d19decc009c
1Shaiza <154394911+1Shaiza@users.noreply.github.com>
2023-12-29 08:41:54 -0600

    Update BoothsnipeWebRaw.lua

--- BoothsnipeWebRaw.lua
+++ BoothsnipeWebRaw.lua
@@ -1,39 +1,19 @@
-local Booths_Broadcast = game:GetService("ReplicatedStorage").Network:WaitForChild("Booths_Broadcast")
 local Exclusives = require(game.ReplicatedStorage:WaitForChild('Library'))
 
-local function checklisting(uid, gems, item, version, shiny, amount, username, playerid)
-    gems = tonumber(gems)
-
-    if string.find(item, "Huge") and gems <= 1000000 then
-        game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
-        processListingInfo(uid, gems, item, version, shiny, amount, username)
-
-    elseif typee.exclusiveLevel and itemdata['class'] == "Pet" and gems <= 5000 then
-        game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
-
-    end
-end
-
+local Booths_Broadcast = game:GetService("ReplicatedStorage").Network:WaitForChild("Booths_Broadcast")
 Booths_Broadcast.OnClientEvent:Connect(function(username, message)
-    local playerID = message['PlayerID']
-    if type(message) == "table" then
-        local listing = message["Listings"]
-        for key, value in pairs(listing) do
-            if type(value) == "table" then
-                local uid = key
-                local gems = value["DiamondCost"]
-                local itemdata = value["ItemData"]
-
-                if itemdata then
-                    local data = itemdata["data"]
-
-                    if data then
-                        local item = data["id"]
-                        local version = data["pt"]
-                        local shiny = data["sh"]
-                        local amount = data["_am"]
-                        checklisting(uid, gems, item, version, shiny, amount, username , playerID , itemdata)
-                    end
+    if type(message) == "table" and message['PlayerID'] then
+        local playerID = message['PlayerID']
+        for key, value in message["Listings"] do
+            local uid = key
+            local data = value["ItemData"]["data"]
+            local item = data["id"]
+            local gems = value["DiamondCost"]
+            local amount = tonumber(data["_am"]) or 1
+            local exclusive = false
+            if class == "Pet" then
+                if exclusive = Exclusives.Directory.Pets[item].exclusiveLevel and gems <= 5000 then
+                    game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerID, uid)
                 end
             end
         end
